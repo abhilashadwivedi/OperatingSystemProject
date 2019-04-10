8724feab36364480b2f97a2bcf208f483e4a5aa5
Basant Singh <Basantmhr@users.noreply.github.com>
2019-04-10 22:53:06 +0530

    changes done
    changes in parameter

--- Operating_System_Multi_Threading_Sorting_Application.c
+++ Operating_System_Multi_Threading_Sorting_Application.c
@@ -5,23 +5,23 @@
 
 int N = 2 ;                                               //storing number of threads in N
 
-int arr[] = {80,77,21,1,2,46,45,87,0,11,23};
+int arr[] = {10,9,8,7,6,5,4,3,2,1};
 
-typedef struct ArrayInput
+typedef struct Arraystruct
 {
     int highest,lowest;
 } ArrayInput;
 
-void merge(int highestInt, int lowestInt)                               //sorting algorithm and merging function
+void merge(int highestElement, int LowestElement)                               //sorting algorithm and merging function
 {
-        int midInt = (lowestInt+highestInt)/2;
-        int left = lowestInt;
-        int right = midInt+1;
+        int midElement = (LowestElement+highestElement)/2;
+        int left = LowestElement;
+        int right = midElement+1;
 
-        int bArray[highestInt-lowestInt+1];
+        int bArray[highestElement-LowestElement+1];
         int temp = 0;
 
-        while(left <= midInt && right <= highestInt)
+        while(left <= midElement && right <= highestElement)
         {
                 if (arr[left] > arr[right])
                         bArray[temp++] = arr[right++];
@@ -29,21 +29,21 @@ void merge(int highestInt, int lowestInt)                               //sortin
                         bArray[temp++] = arr[right++];
         }
 
-        while(left <= midInt)
+        while(left <= midElement)
         bArray[temp++] = arr[left++];
 
-        while(right <= highestInt)
+        while(right <= highestElement)
         bArray[temp++] = arr[left++];
 
-        for (int i = 0; i < (highestInt-lowestInt+1) ; i++)
-        arr[lowestInt+i] = bArray[i];
+        for (int i = 0; i < (highestElement-LowestElement+1) ; i++)
+        arr[LowestElement+i] = bArray[i];
 }
 
-void * mergesort(void *a)                                   
+void *mergesort(void *a)                                   
 {
         ArrayInput *newArrayInput = (ArrayInput *)a;
         int mid = (newArrayInput->lowest + newArrayInput->highest)/2;
-
+        
         ArrayInput aIndex[N];
         pthread_t thread[N];
 
@@ -53,15 +53,17 @@ void * mergesort(void *a)
         aIndex[1].lowest = mid+1;
         aIndex[1].highest = newArrayInput->highest;
 
-        if (newArrayInput->lowest >= newArrayInput->highest) return 0;
-        int i=0;
+        if (newArrayInput->lowest >= newArrayInput->highest) 
+            return 0;
+
+        int i;
         for(i = 0; i < N; i++)
         pthread_create(&thread[i], NULL, mergesort, &aIndex[i]);    
 
         for(i = 0; i < N; i++)
         pthread_join(thread[i], NULL);                              
 
-        merge(newArrayInput->lowest, newArrayInput->highest);
+        merge(newArrayInput->highest, newArrayInput->lowest);
    
 }
 
@@ -69,11 +71,12 @@ void main()
 {
         ArrayInput SortingArray;                                                   
         SortingArray.lowest = 0;
-        SortingArray.highest = sizeof(arr)/sizeof(arr[0])-1;                         
+        SortingArray.highest = sizeof(arr)/sizeof(arr[0])-1;
+       //printf("%d\n",SortingArray.highest );                         
         pthread_t threadSorting;                                           
 
-        pthread_create(&threadSorting, NULL, mergesort, &SortingArray);              
+        pthread_create(&threadSorting, NULL,&mergesort, &SortingArray);              
         pthread_join(threadSorting, NULL);                                 
         for (int i = 0; i < 10; i++)
-      	printf("%d\n", arr[i]);
+      	printf("%d\t", arr[i]);
 }
